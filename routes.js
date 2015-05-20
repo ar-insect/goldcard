fb9279b47030a48d1b5ccb8db2ea2b4791cc6e38
ar-insect <ar.insect@gmail.com>
2015-05-20 18:45:19 +0800

    第二版

--- routes.js
+++ routes.js
@@ -3,13 +3,14 @@
  */
 var site = require('./controllers/home'); // default index.js
 
+var testdata = require('./controllers/home/data'); //test data
 // more controllers define here.
 // ...
 
 module.exports = function(app) {
     // home
     app.get('/', site.index);
-
+    app.get('/home/testdata.json', testdata.combotree);
     // 404 page warn: must in the last
     app.get('*', function(req, res) {
         res.render('home/404', {
