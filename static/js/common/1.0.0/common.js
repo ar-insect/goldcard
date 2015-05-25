6c9c3caa39c99b2db8b3dca055189d867c7e0550
ar-insect <ar.insect@gmail.com>
2015-05-25 10:39:26 +0800

    updage

--- static/js/common/1.0.0/common.js
+++ static/js/common/1.0.0/common.js
@@ -2,82 +2,82 @@ if (window.jQuery) {
     $(document).ready(function () {
 
         // NAVIGATION MENU
-		$('ul.drawers').accordion({
-			header: 'H2.drawer-handle',
-			selectedClass: 'open',
-			event: 'click'
-		});
+        $('ul.drawers').accordion({
+            header: 'H2.drawer-handle',
+            selectedClass: 'open',
+            event: 'click'
+        });
 
-		var tabpanel, panels = [];
-		var config = {};
-		var store = {};
-		var pre = 'PANEL';
-		var id = 1;
-		// ³õÊ¼»¯¼ÓÔØÄ³¸öÃæ°å
-		tabpanel = new TabPanel({
-			renderTo:'ui-tabs',
-			width:'auto',
-			//height:'1000px',
-			border:'none',
-			active : 0,
-			//maxLength : 10,
-			items : []
-		});
+        var tabpanel, panels = [];
+        var config = {};
+        var store = {};
+        var pre = 'PANEL';
+        var id = 1;
+        // åˆå§‹åŒ–åŠ è½½æŸä¸ªé¢æ¿
+        tabpanel = new TabPanel({
+            renderTo:'ui-tabs',
+            width:'auto',
+            //height:'1000px',
+            border:'none',
+            active : 0,
+            //maxLength : 10,
+            items : []
+        });
 
-		var getPanel = function(key) {
-			for (var i in store) {
-				if (key === i) return store[i];
-			}
-			return null;
-		};
+        var getPanel = function(key) {
+            for (var i in store) {
+                if (key === i) return store[i];
+            }
+            return null;
+        };
 
-		var setPanel = function(key, obj) {
-			store[key] = obj;
-		};
+        var setPanel = function(key, obj) {
+            store[key] = obj;
+        };
 
-		var enableClose = function() {
-			var tabs = tabpanel.tabs;
-			for(var i = 0, len = tabs.length; i < len; i++) {
-				tabpanel.setClosable(tabs[i].id, false);
-			}
-		};
+        var enableClose = function() {
+            var tabs = tabpanel.tabs;
+            for(var i = 0, len = tabs.length; i < len; i++) {
+                tabpanel.setClosable(tabs[i].id, false);
+            }
+        };
 
-		tabpanel.killCallback = function(obj) {
-			var key = obj[0].key;
-			store[key] = null;
-			if (this.tabs.length == 1) {
-				// ×îºóÒ»¸ö½ûÓÃ¹Ø±Õ
-				tabpanel.setClosable(this.tabs[0].id, true);
-			}
-		};
+        tabpanel.killCallback = function(obj) {
+            var key = obj[0].key;
+            store[key] = null;
+            if (this.tabs.length == 1) {
+                // æœ€åä¸€ä¸ªç¦ç”¨å…³é—­
+                tabpanel.setClosable(this.tabs[0].id, true);
+            }
+        };
 
-		$('#J_sidebox .drawer li a').on('click', function(e) {
-			e.preventDefault();
-			var panelid, panel;
-			var title = $(this).text() || '';
-			var url = $(this).attr('data-url');
-			var key = $(this).attr('data-key');
-			if(!url || !key) return;
-			if (panel = getPanel(key)) {
-				// Èç¹ûÒÑ¾­´ò¿ª¹ı´ËÃæ°åÔòÖ±½Ó¼¤»î
-				tabpanel.show(tabpanel.getTabPosision(panel.id), false);
-				tabpanel.refresh(panel.id);
-			} else {
-				// ·ñÔòĞÂ¿ªÃæ°å
-				panelid = pre + id++;
-				config = {
-					key: key,
-					id: panelid,
-					title: title,
-					//height:'1000px',
-					html: '<iframe src="' + url + '" id="' + panelid + 'Frame" width="100%" height="100%" frameborder="0"></iframe>',
-					closable: true
-				};
-				tabpanel.addTab(config);
-				setPanel(key, tabpanel.getActiveTab());
-			}
-			if (tabpanel.tabs.length > 1) enableClose();
-		});
+        $('#J_sidebox .drawer li a').on('click', function(e) {
+            e.preventDefault();
+            var panelid, panel;
+            var title = $(this).text() || '';
+            var url = $(this).attr('data-url');
+            var key = $(this).attr('data-key');
+            if(!url || !key) return;
+            if (panel = getPanel(key)) {
+                // å¦‚æœå·²ç»æ‰“å¼€è¿‡æ­¤é¢æ¿åˆ™ç›´æ¥æ¿€æ´»
+                tabpanel.show(tabpanel.getTabPosision(panel.id), false);
+                tabpanel.refresh(panel.id);
+            } else {
+                // å¦åˆ™æ–°å¼€é¢æ¿
+                panelid = pre + id++;
+                config = {
+                    key: key,
+                    id: panelid,
+                    title: title,
+                    //height:'1000px',
+                    html: '<iframe src="' + url + '" id="' + panelid + 'Frame" width="100%" height="100%" frameborder="0"></iframe>',
+                    closable: true
+                };
+                tabpanel.addTab(config);
+                setPanel(key, tabpanel.getActiveTab());
+            }
+            if (tabpanel.tabs.length > 1) enableClose();
+        });
 
     });
 }
